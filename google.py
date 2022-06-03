b8b70a9be8e3d747e138a544afa8383372756963
YeonJae <94735529+yeonvv@users.noreply.github.com>
2022-06-03 12:07:47 +0900

    Add files via upload

---
+++ google.py
@@ -0,0 +1,48 @@
+from itertools import count
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import urllib.request 
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
+elem = driver.find_element(By.NAME, "q")
+elem.send_keys("김연아")
+elem.send_keys(Keys.RETURN)
+
+SCROLL_PAUSE_TIME = 1
+last_height = driver.execute_script("return document.body.scrollHeight")
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(SCROLL_PAUSE_TIME)
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        try:
+            driver.find_element(By.CSS_SELECTOR, ".mye4qd").click()
+        except:
+            break
+    last_height = new_height
+
+images = driver.find_elements(By.CSS_SELECTOR,".rg_i.Q4LuWd")
+count = 1
+for image in images:
+    try:
+        image.click()
+        time.sleep(2)
+        imgUrl = driver.find_element(By.XPATH, "/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[3]/div/a/img").get_attribute("src")
+        urllib.request.urlretrieve(imgUrl, f"{str(count)}.jpg")
+        count = count + 1
+    except:
+        pass
+
+driver.close()
+# assert "Python" in driver.title
+# elem = driver.find_element(By.NAME, "q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close()
